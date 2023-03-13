cd4628ef64868e05d84a4b006902b0dc91263e45
Marcin Mierzejewski <mierzejewski2you@gmail.com>
2023-03-13 23:24:57 +0100

    add media screen to hero

--- src/components/Hero/Hero.styled.js
+++ src/components/Hero/Hero.styled.js
@@ -3,60 +3,89 @@ import heroImg from "./hero2.jpg";
 
 export const HeroWrapper = styled.div`
   display: flex;
-  text-align: left;
-  justify-content: left;
+  text-align: center;
+  justify-content: center;
   max-width: 1280px;
   margin: 0 auto;
   padding: 0 15px 50px;
-  
 `;
 
 export const HeroLeft = styled.div`
-width: 50%;
-padding-top: 50px;
-padding-bottom: 50px;
+  width: 100%;
+  padding-top: 50px;
+  padding-bottom: 50px;
 
-> h1, h2 {
-  font-family: 'Playfair Display';
-  width: 900px;
-  font-size: 100px;
-  line-height: 120%;
-  position: relative;
-  z-index: 1;
-}
+  @media screen and (min-width: 768px) {
+    width: 50%;
+    text-align: left;
+    justify-content: left;
+  }
 
-> h2 {
-  font-size: 80px;
-  line-height: 120%;
-  font-weight: 500;
-}
+  > h1,
+  h2 {
+    font-family: "Playfair Display";
+    font-size: 70px;
+    line-height: 120%;
+    position: relative;
+    z-index: 1;
 
-> h3 {
-  font-size: 60px;
-  line-height: 120%;
-  font-weight: 500;
-  margin-top: 30px;
-}
+    @media screen and (min-width: 768px) {
+      width: 650px;
+      font-size: 100px;
+    }
+  }
 
-> p {
-  font-size: 30px;
-  line-height: 180%;
-  opacity: 0.5;
-}
+  > h2 {
+    font-size: 50px;
+    line-height: 120%;
+    font-weight: 500;
+    @media screen and (min-width: 768px) {
+      font-size: 80px;
+    }
+  }
 
-`
+  > h3 {
+    font-size: 36px;
+    line-height: 120%;
+    font-weight: 500;
+    margin-top: 30px;
+    @media screen and (min-width: 768px) {
+      font-size: 60px;
+    }
+  }
+
+  > p {
+    font-size: 22px;
+    line-height: 180%;
+    opacity: 0.5;
+    @media screen and (min-width: 768px) {
+      font-size: 30px;
+    }
+  }
+`;
+
 export const HeroRight = styled.div`
   background-color: red;
-  width: 50%;
+  display: none;
   /* height: 300px; */
   background-size: cover;
-  background-repeat:no-repeat;
+  background-repeat: no-repeat;
   background-position: 50% 50%;
   background-image: url(${heroImg});
   filter: sepia(60%);
+
+  @media screen and (min-width: 768px) {
+    display: block;
+    width: 50%;
+  }
 `;
 
 export const ButtonWrapper = styled.div`
   display: flex;
+  justify-content: center;
   margin-top: 50px;
-`
+
+  @media screen and (min-width: 768px) {
+    justify-content: left;
+  }
+`;
