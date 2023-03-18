1d15e595dd970815c235f9ba983f5c2c4e05dc56
Marcin Mierzejewski <mierzejewski2you@gmail.com>
2023-03-18 20:03:44 +0100

    add media screen for ProhectItem

--- src/components/ProjectItem/ProjectItem.styled.js
+++ src/components/ProjectItem/ProjectItem.styled.js
@@ -1,27 +1,42 @@
 import styled from "styled-components";
 
+export const ProjectContent = styled.div`
+  background-color: red;
+  padding: 30px;
+`;
+
 export const ProjectWrapper = styled.li`
   display: flex;
+  flex-direction: column;
   align-items: center;
-  width: 560px;
-  height: 300px;
+  justify-content: space-between;
+  /* width: 560px; */
+  width: 80vw;
+  max-width: 540px;
+  /* height: 300px; */
   background-color: var(--color-content);
   border-radius: 16px;
+  @media screen and (min-width: 768px) {
+    flex-direction: row;
+    height: 300px;
+    /* width: 100%; */
+  }
 `;
 
 export const ImageBox = styled.div`
   overflow: hidden;
   height: 250px;
-  width: 200px;
-  margin-left: -25px;
+  width: 90%;
+  max-width: 500px;
+  margin-top: -25px;
   border-radius: 16px;
   box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.12), 0px 1px 1px rgba(0, 0, 0, 0.14),
     0px 2px 1px rgba(0, 0, 0, 0.2);
-`;
-
-export const ProjectContent = styled.div`
-  /* background-color: red; */
-  padding: 30px;
+  @media screen and (min-width: 768px) {
+    margin-top: 0;
+    margin-left: -25px;
+    width: 200px;
+  }
 `;
 
 export const ProjectImage = styled.img`
@@ -36,10 +51,11 @@ export const ProjectImage = styled.img`
 `;
 
 export const ContentBox = styled.div`
-  padding-left: 40px;
+  padding-left: 20px;
   padding-right: 20px;
   display: flex;
-  width: 100%;
+  width: 70%;
+  /* width: 100%; */
   height: 100%;
   flex-direction: column;
   justify-content: space-around;
